0f1b652b2d599b42865ea1a631ab0a453590f060
中村　麟太郎 <hemisphere.babylon@icloud.com>
2016-11-27 18:36:44 +0900

    クライアントからPOSTされたuuidをDBに保存

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -1,4 +1,29 @@
+require('securerandom')
+
 class ApiKey < ApplicationRecord
   belongs_to :user
   validates :access_token, presence: true
+
+  STATUS = {
+    disabled: 0,
+    enabled: 1
+  }
+
+  def generate_access_token
+    begin
+      self.access_token = SecureRandom.hex
+    end while self.class.exists?(access_token: access_token)
+  end
+
+  class << self
+    # 初期化されたインスタンスを生成
+    def generate
+      api_key = ApiKey.new
+      api_key.generate_access_token
+      api_key.status = ApiKey::STATUS[:enabled]
+      # これを忘れて1時間苦しんだ。。。
+      api_key
+    end
+  end
+
 end
